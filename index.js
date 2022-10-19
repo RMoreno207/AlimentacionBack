b1fe8647a9356295e9841676ae405bd895b5c912
RMoreno207 <107397146+RMoreno207@users.noreply.github.com>
2022-10-19 23:38:06 +0200

    refactorized

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 var corsOptions = {
-    origin: 'https://alimentacion.vercel.app/',
+    origin: 'https://alimentacion.vercel.app',
     credentials: true
 }
 //Router
