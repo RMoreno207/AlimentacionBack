ec697a83579b36643182ccead485e570faf45373
RMoreno207 <107397146+RMoreno207@users.noreply.github.com>
2023-03-31 13:50:18 +0200

    restart app

--- index.js
+++ index.js
@@ -1,40 +1,40 @@
-const express = require('express');
-const path = require('path');
-require('dotenv').config();
-const cookieParser = require('cookie-parser')
-const morgan = require('morgan');
+const express = require("express");
+const path = require("path");
+require("dotenv").config();
+const cookieParser = require("cookie-parser");
+const morgan = require("morgan");
 // const helmet = require('helmet');
-const cors = require('cors');
-require('./utils/dbSQL.js');
+const cors = require("cors");
+require("./utils/dbSQL.js");
 const app = express();
 const port = process.env.PORT || 5000;
 
 var corsOptions = {
-    origin: ['https://alimentacion.vercel.app', "http://localhost:3000"],
-    credentials: true
-}
+  origin: ["https://alimentacion.vercel.app", "http://localhost:3000"],
+  credentials: true,
+};
 //Router
-const userRouter = require('./routes/userRoutes.js');
+const userRouter = require("./routes/userRoutes.js");
 // const router = require('./routes/routes');
 
-//Middlewares
-const middle404 = require('./middlewares/error404.js');
+//Middlewaress
+const middle404 = require("./middlewares/error404.js");
 
 //Rutas
 // app.use(helmet());
-app.set('trust proxy', 1);
+app.set("trust proxy", 1);
 app.use(express.json());
 app.use(cors(corsOptions));
 // app.use('/', router);
-app.use('/api/', userRouter);
-app.use(cookieParser())
+app.use("/api/", userRouter);
+app.use(cookieParser());
 
 //Read body request
 
-
-app.use(express.json())// Para habilitar recepción de datos JSON en una request //Es necesario??
+app.use(express.json()); // Para habilitar recepción de datos JSON en una request //Es necesario??
 app.use(express.urlencoded({ extended: true }));
-const loggerFormat = ':method :url :status :response-time ms - :res[content-length]'
+const loggerFormat =
+  ":method :url :status :response-time ms - :res[content-length]";
 // app.use(morgan(loggerFormat, {
 //     skip: function (req, res) {
 //         return res.statusCode < 400
@@ -43,15 +43,15 @@ const loggerFormat = ':method :url :status :response-time ms - :res[content-leng
 // }));
 
 // Serve the static files from the React app
-app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.static(path.join(__dirname, "client/build")));
 
 // Handles any requests that don't match the ones above
-app.get('*', function (req, res) {
-    const index = path.join(__dirname, 'build', 'index.html');
-    res.sendFile(index);
+app.get("*", function (req, res) {
+  const index = path.join(__dirname, "build", "index.html");
+  res.sendFile(index);
 });
 
 app.use(middle404);
 app.listen(port);
 
-console.log('App is listening on port ' + port);
+console.log("App is listening on port " + port);
